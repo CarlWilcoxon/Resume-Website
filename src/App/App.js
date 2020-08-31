a68fc05d4bf9533557d3c76ae1e1fa753a87b6d9
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-08-30 21:17:38 -0500

    Initial setup

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <a
+            className="Home"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Carl Wilcoxon
+          </a>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
